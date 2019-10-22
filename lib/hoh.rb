2c4de1d41196cf0d6e509607d1b7a5e1b57b80b0
Joseph  Louie <joseph161216@gmail.com>
2019-10-22 00:52:11 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,8 +46,11 @@ def naming_system
     sub_category: {
       label: "Phylum",
       sub_category: {
-        label: "Class"
+        label: "Class",
+        sub_category: {
+          label: "Order"
+        }
       }
     }
-}
+  }
 end
